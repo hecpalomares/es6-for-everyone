0085ef720b66c01d0d1c7ade0f9d9fc8db75d535
hecpalomares <hpalomares12@gmail.com>
2017-07-17 23:42:20 -0500

    Gitignore + Ep1

---
+++ ep1_var-let-const.js
@@ -0,0 +1,14 @@
+
+function setWidth () {
+	var width = 100;
+	console.log('setWidth: ', width)
+}
+
+setWidth();
+
+var age = 100;
+
+if(age > 12) {
+	const dogYears = age * 7;
+	console.log(`You age at DogYears is ${dogYears}`);
+}
